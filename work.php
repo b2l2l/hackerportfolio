74416080da0311a4910dc4cedb5a29e864ae2f54
b2l2l <bilalbarzinji@gmail.com>
2015-01-01 02:01:46 +0300

    0.4

---
+++ work.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+    <head>
+        <meta charset=utf-8>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>my works b2l2l.com</title>
+        <link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700&amp;subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+        <link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.css" />
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+    </head>
+    
+    <body>
+        <div class="navbar">
+            <div class="navbar-inner">
+                <div class="container">
+               
+                    <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <i class="icon-menu"></i>
+                    </button>
+                    <div class="nav-collapse collapse pull-right">
+                        <ul class="nav" id="top-navigation">
+                            <li class="active"><a href="#home">my Work</a></li>
+                            <li><a href="./index.php">Back to the shell</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="section primary-section" id="about">
+            <div class="triangle"></div>
+            <div class="container">
+                <div class=" title">
+                    <h1>Have You Seen my Works?</h1>
+                    <p>Here are some projects I did  .</p>
+                </div>
+                
+                <div id="single-project">
+                    <div id="slidingDiv" class="toggleDiv row-fluid single-project">
+                        <div class="span6">
+                            <img src="images/Portfolio01.png" alt="project 1" />
+                        </div>
+                        <div class="span6">
+                            <div class="project-description">
+                                <div class="project-title clearfix">
+                                    <h3> website for BreadHouse Bakery </h3>
+
+                                </div>
+                                <div class="project-info">
+                                    <div>
+                                        <span>Client</span>Breadhouse</div>
+                                    <div>
+                                        <span>Date</span>Dec 2014</div>
+                                    <div>
+                                        <span>Skills</span>HTML,CSS</div>
+                                    <div>
+                                        <span>Link</span>www.bhbakery.com</div>
+                                </div>
+                                <p>Bread house is an owner-operated, independent bakery offering European/ Arabic -style breads, all made in Iraq/ Erbil.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="slidingDiv1" class="toggleDiv row-fluid single-project">
+                        <div class="span6">
+                            <img src="images/Portfolio02.png" alt="project 2">
+                        </div>
+                        <div class="span6">
+                            <div class="project-description">
+                                <div class="project-title clearfix">
+                                    <h3>website for Soma City </h3>
+
+                                </div>
+                                <div class="project-info">
+                                    <div>
+                                        <span>Client</span>Soma city</div>
+                                    <div>
+                                        <span>Date</span>July 2014</div>
+                                    <div>
+                                        <span>Skills</span>HTML5, CSS3, JavaScript</div>
+                                    <div>
+                                        <span>Link</span>www.soma-city.com</div>
+                                </div>
+                                <p><  يعتبر مشروع سوما ستي السكني من المشاريع المهمة في مدينة كركوك نظرا لموقعه الجغرافي المتميز في طبيعة   >.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="slidingDiv2" class="toggleDiv row-fluid single-project">
+                        <div class="span6">
+                            <img src="images/Portfolio03.png" alt="project 3">
+                        </div>
+                        <div class="span6">
+                            <div class="project-description">
+                                <div class="project-title clearfix">
+                                    <h3>BugReport-Security Researching </h3>
+
+                                </div>
+                                <div class="project-info">
+                                    <div>
+                                        <span>Client</span>"HIDDEN"</div>
+                                    <div>
+                                        <span>Date</span>00:00</div>
+                                    <div>
+                                        <span>Skills</span>Hacking , Securing </div>
+                                    
+                                </div>
+                                <p>i Don't only do Creative websites! I spend a part of my  Free time to scan website from bugs/Vulns report them, then probably work for the company securing the online-Section  .</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="slidingDiv3" class="toggleDiv row-fluid single-project">
+                        <div class="span6">
+                            <img src="images/Portfolio04.png" alt="project 4">
+                        </div>
+                        <div class="span6">
+                            <div class="project-description">
+                                <div class="project-title clearfix">
+                                    <h3>Translating volunteering </h3>
+
+                                </div>
+                                <div class="project-info">
+                                    <div>
+                                        <span>Client</span>The amazing WINRAR.</div>
+                                    <div>
+                                        <span>Date</span>DEC 2014 - 2015</div>
+                                    <div>
+                                        <span>Skills</span>HTML, basics of VB, Basics of programming</div>
+                                    <div>
+                                        <span>Link</span>www.kr-winrar.com</div>
+                                </div>
+                                <p> well i spend part of my free time volunteering translating famous and used programs to KURDISH[mylang] ,I'm currently working on making offical winrar kurdish version .</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="slidingDiv4" class="toggleDiv row-fluid single-project">
+                        <div class="span6">
+                            <img src="images/Portfolio05.png" alt="project 5">
+                        </div>
+                        <div class="span6">
+                            <div class="project-description">
+                                <div class="project-title clearfix">
+                                    <h3>Social Media promoting-SEO</h3>
+
+                                </div>
+                                <div class="project-info">
+                                    <div>
+                                        <span></span></div>
+                                    <div>
+                                        <span>Date</span>2014-2015</div>
+                                    <div>
+                                        <span>Skills</span>SEO knowledge[sitemap...etc] ,Socail media</div>
+                                    <div>
+                                        <span></span></div>
+                                </div>
+                                <p>Yes I do both Seo[Search Engine optimization] I can get you up-ranked in google,yahoo,bing </br> the Social Media promotions are 2option it's or a one time promotion or yearly full manage .</p>
+                            </div>
+                        </div>
+                    </div>
+                   
+                   
+               
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+            <p>&copy; 2015 All Rights Reserved</p>
+        </div>
+        
+    </body>
+</html>
