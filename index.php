e7825b40a3d910fcb3c43bb74c10df3f54d76f50
b2l2l <bilalbarzinji@gmail.com>
2015-01-03 01:27:22 +0300

    0.9.1
    Huge Upgrade . Skills.css added .
    code commented .
    footer bug fixed
    w3 code valadation .
    
    Cleaner code . Faster
    2days before my finals exams :(

--- index.php
+++ index.php
@@ -3,27 +3,29 @@
 <html lang="en">
 
 <head>
-
+	
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
- <head>
- 
-  <title>guest@b2l2l</title>
-  <link rel="stylesheet" type="text/css" href="style.css" media="screen" title="Default" />
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+    <meta name="description" content="Hacker Portfolio coded by Bilal barzinji . - Personal website of Bilal . ">
+    <meta name="author" content="Bilal Barzzinji">
+
+ <!-- Page title line 14 , line 15 is includeing the css code , line 16includeing j-qeury lib -->
+		<title>Bilal Barzinji - guest@b2l2l.com</title>
+		<link rel="stylesheet" type="text/css" href="./style.css" media="screen" title="Default" />
+		<link rel="stylesheet" type="text/css" href="./css/skills.css" media="screen" title="Default" />
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
   
  </head>
- <body><a href="./index.php">
- <img src='./logo.png' width='200px'  > </a>
-  <?php
+ <body>
+			<a href="./index.php"><img src='./logo.png' width='200' alt='logo of hacker portfolio'></a>
+		<?php
 	$cmd = $_GET["o"];
 		if ($cmd == "") 
 						{
-			echo " </br><font color='white' >#NEVER TRY TO HACK A HACKER </br>enter '<font color='red'>mywork' </font>to see my work 
-			</br> enter '<font color='pink'>bilal'</font> to read more about me </font>
+			echo " <br><p>#NEVER TRY TO HACK A HACKER <br>enter '<font color='red'>mywork' </font>to see my work 
+			<br> enter '<font color='pink'>bilal'</font> to read more about me <br>
+			enter '<font color='pink'>skills'</font> you will see what Bilal is good at .</p> </font>
 ";
 											}
 		elseif($cmd == "mywork"  || $cmd=="Mywork" || $cmd=="my work" || $cmd=="My Work" || $cmd=="My work" || $cmd=="MYWORK") 
@@ -60,23 +62,20 @@
 		echo " <p> don't Try to be smart and enter something i didn't tell you about .</p> " ;
 		     }
 											
-?>
-  </br></br></br></br></br></br></br></br></br></br></br>
+				?>
+  
 <form method="GET">
-<font color='white' >root@b2l2l:</font> <input type="text" name="o"><br>
+<p>root@b2l2l: <input type="text" name="o"></p><br>
 </form>
 
 
 
 
-
-
-
-
-
-
-
-  <div id="credit">coded by <a href="http://b2l2l.com">Bilal barzinji</a>.<a href="http://github.com/b2l2l/hackerportfolio">Source code on github</a>.</div>
- </body
+<script>
+<!-- 
+document.write(unescape("%3Cscript%3E%0A%3C%21--%0Adocument.write%28unescape%28%22%250A%253C%2521--%2520footer%2520.%2520please%2520keep%2520my%2520rights%2520or%2520at%2520least%2520star%2520the%2520project%2520on%2520github%2520.%2520--%253E%2520%250A%2509%253C/br%253E%253C/br%253E%250A%2520%2520%253Cdiv%2520id%253D%2522credit%2522%253Ecoded%2520by%2520%253Ca%2520href%253D%2522http%253A//b2l2l.com%2522%253EBilal%2520barzinji%253C/a%253E.%253Ca%2520href%253D%2522http%253A//github.com/b2l2l/hackerportfolio%2522%253ESource%2520code%2520on%2520github%253C/a%253E.%253C/div%253E%250A%2520%22%29%29%3B%0A//--%3E%0A%3C/script%3E"));
+//-->
+</script>
+</body>
 </html>
 
